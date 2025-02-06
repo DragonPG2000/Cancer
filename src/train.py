15c1c53e27f45cd1a1679332327f59d9bea55538
Narayanan_Sunil_Kumar_skumar704 <skumar704@celdl0205yc01d.ce.gatech.edu>
2025-02-06 17:52:58 -0500

    Cleaned up

--- src/train.py
+++ src/train.py
@@ -12,12 +12,37 @@ from PIL import Image
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from tqdm import tqdm
 import argparse
+import cv2
+import numpy as np
+from PIL import Image
+
+def hair_remove(image):
+    """
+    Remove hair from the image using inpainting (Source: https://www.kaggle.com/code/youssefhatem1/melanoma-hair-remove)
+    """
+    # convert image to grayScale
+    grayScale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    
+    # kernel for morphologyEx
+    kernel = cv2.getStructuringElement(1,(17,17))
+    
+    # apply MORPH_BLACKHAT to grayScale image
+    blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel)
+    
+    # apply thresholding to blackhat
+    _,threshold = cv2.threshold(blackhat,10,255,cv2.THRESH_BINARY)
+    
+    # inpaint with original image and threshold image
+    final_image = cv2.inpaint(image,threshold,1,cv2.INPAINT_TELEA)
+    
+    return final_image
 
 # Define data transformations
 data_transforms = {
     'train': transforms.Compose([
         transforms.Lambda(lambda img: Image.fromarray(hair_remove(np.array(img)))),
         transforms.Resize((224, 224)),
+        # transforms.Lambda(lambda img: Image.fromarray(hair_remove(np.array(img)))),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
